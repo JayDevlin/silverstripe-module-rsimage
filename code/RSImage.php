92ed0754681f01e515992487e92f9d384c25cf4e
Devlin <support@layon.de>
2013-02-19 11:13:28 +0100

    Remove cached formatted images on delete

--- code/RSImage.php
+++ code/RSImage.php
@@ -22,5 +22,54 @@ class RSImage extends Image {
 		return $folder . "_" . $format . "/" . $this->Name;
 		//return $folder . "_resampled/$format-" . $this->Name;
 	}
+	
+	protected function onBeforeDelete() {
+		parent::onBeforeDelete();
+		
+		$this->deleteFormattedRSImages();
+	}
+	
+	/**
+	 * Remove all of the formatted cached images for this image.
+	 * @return array - paths of deleted formatted images
+	 */
+	function deleteFormattedRSImages() {
+		if(!$this->Filename) return array();
+		
+		$filesDeleted = array();
+		$methodNames = array_merge($this->allMethodNames(), get_class_methods('RSImage'));
+		$cacheFolders = array();
+		
+		$folderName = $this->ParentID ? $this->Parent()->Filename : ASSETS_DIR . '/';
+		$folder = Director::getAbsFile($folderName);
+		
+		if(is_dir($folder)) {
+			if($handle = opendir($folder)) {
+				while(($file = readdir($handle)) !== false) {
+					if(substr($file, 0, 1) == '_') {
+						$cacheFolders[] = $file;
+					}
+				}
+				closedir($handle);
+			}
+		}
+		
+		foreach($methodNames as $methodName) {
+			if(substr($methodName, 0, 8) == 'generate') {
+				$format = substr($methodName, 8);
+				foreach($cacheFolders as $cacheFolder) {
+					if(strstr($cacheFolder, '_'.$format)) {
+						$cacheFolder = Director::getAbsFile($folderName . $cacheFolder . '/');
+						if(Director::fileExists($cacheFolder . $this->Name)) {
+							unlink($cacheFolder . $this->Name);
+							$filesDeleted[] = $cacheFolder . $this->Name;
+						}
+					}
+				}
+			}
+		}
+		
+		return $filesDeleted;
+	}
 
 }
