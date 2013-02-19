421a82f08e961d36648ab3e0ad3261a8ded44bd7
Devlin <support@layon.de>
2013-02-19 11:15:59 +0100

    Remove cached formatted images on upload

--- code/RSImageDecorator.php
+++ code/RSImageDecorator.php
@@ -4,9 +4,21 @@
  * - RSImageDecorator -
  * 
  * Change ClassName to RSImage after a image has been uploaded
+ * 
+ * @see Image->onAfterUpload()
+ * @see AssetAdmin->doUpload()
  */
 class RSImageDecorator extends DataObjectDecorator {
+	/**
+	 * Should be called after the file was uploaded
+	 */
 	function onAfterUpload() {
-		DB::query("UPDATE File SET ClassName = 'RSImage' WHERE ClassName = 'Image';");
+		$imageObj = $this->owner;
+		if( !empty($imageObj) ) {
+			DB::query("UPDATE File SET ClassName = 'RSImage' WHERE ID = '".(int)$imageObj->ID."'");
+			
+			$rsImageObj = DataObject::get_by_id('RSImage', (int)$imageObj->ID);
+			$rsImageObj->deleteFormattedRSImages();
+		}
 	}
 }
